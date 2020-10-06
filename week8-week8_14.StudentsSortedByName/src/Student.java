74ca0668087bbdaee7b56b6db8a15ca9b74a9a4a
rohanap <36108401+rohanap@users.noreply.github.com>
2020-10-06 12:29:19 +0530

    Add files via upload

---
+++ week8-week8_14.StudentsSortedByName/src/Student.java
@@ -0,0 +1,27 @@
+public class Student implements Comparable<Student>{
+
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Student s) {
+        
+        //return this.name.compareTo(s.name);
+        return  this.name.compareToIgnoreCase(s.getName());
+    }
+    
+    
+
+}
